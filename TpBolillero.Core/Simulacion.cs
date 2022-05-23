f00980844c7a640f9a089464db63954ce0cf44d6
JhonnyVicente1019 <jhonnyvicentecolque@gmail.com>
2022-05-23 17:16:56 -0300

    Simulacion

--- TpBolillero.Core/Simulacion.cs
+++ TpBolillero.Core/Simulacion.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace TpBolillero.Core
 {
     public class Simulacion
     {
-        public long simularSinHilos(Bolillero clon, List<byte> jugadas, long cantidad)
+        public long SimularSinHilos(Bolillero clon, List<byte> jugadas, long cantidad)
         {
             return clon.JugarN(jugadas, cantidad);
         }
-        public long simularConHilos(Bolillero clon, List<byte> jugadas, long cantidad, long hilos)
+        public long SimularConHilos(Bolillero clon, List<byte> jugadas, long cantidad, long hilos)
         {
-            Task<long>{}tareas = new Task<long>{hilos};
+            Task<long>[]tareas = new Task<long>[hilos];
+            for(long i = 0 ; i < cantidad; i++)
+            {
+                var nuevoBolillero = (Bolillero)clon.Clone();
+                tareas[i] = Task.Run(() => SimularSinHilos(clon, jugadas, hilos));
+            }
+            var 
         }
     }
 }
